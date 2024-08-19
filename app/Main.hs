093fc34356f0e45a8263bccebf4d00d6832c143b
zhang <1434926171@qq.com>
2024-08-19 20:02:25 +0800

    init

---
+++ app/Main.hs
@@ -0,0 +1,60 @@
+data State = A | B | C | D | E | Halt deriving (Eq, Show)
+
+data Symbol = Zero | One deriving (Eq, Show)
+
+data Direction = L | R deriving (Eq, Show)
+
+type Tape = ([Symbol], Symbol, [Symbol])
+
+type Steps = Int
+
+type WriteCount = Int
+
+-- tape head
+move :: Tape -> Direction -> Tape
+move (ls, s, []) R = (s : ls, Zero, [])
+move (ls, s, r : rs) R = (s : ls, r, rs)
+move ([], s, rs) L = ([], Zero, s : rs)
+move (l : ls, s, rs) L = (ls, l, s : rs)
+
+-- rules
+step :: State -> Symbol -> (Symbol, Direction, State)
+step A Zero = (One, R, B)
+step A One = (One, L, C)
+step B Zero = (One, R, C)
+step B One = (One, R, B)
+step C Zero = (One, R, D)
+step C One = (Zero, L, E)
+step D Zero = (One, L, A)
+step D One = (One, L, D)
+step E Zero = (One, R, Halt)
+step E One = (Zero, L, A)
+step _ _ = error "Invalid state or symbol"
+
+executeStep :: (State, Tape, Steps, WriteCount) -> (State, Tape, Steps, WriteCount)
+executeStep (state, (ls, s, rs), steps, writeCount) =
+  let (writeSymbol, moveDir, nextState) = step state s
+      newTape = move (ls, writeSymbol, rs) moveDir
+      newWriteCount = if writeSymbol == One then writeCount + 1 else writeCount
+   in (nextState, newTape, steps + 1, newWriteCount)
+
+initTape :: Tape
+initTape = ([], Zero, repeat Zero)
+
+runMachine :: (State, Tape, Steps, WriteCount) -> IO ()
+runMachine (Halt, tape, steps, writeCount) = do
+  putStrLn $ "Step: " ++ show steps
+  putStrLn "Machine halted."
+runMachine stateTapeStepsWriteCount@(state, tape, steps, writeCount) = do
+  runMachine $ executeStep stateTapeStepsWriteCount
+
+printTape :: Tape -> IO ()
+printTape (ls, s, rs) = putStrLn $ concatMap showSymbol (reverse ls) ++ showSymbol s ++ take 10000 (concatMap showSymbol rs)
+  where
+    showSymbol Zero = "0"
+    showSymbol One = "1"
+
+main :: IO ()
+main = do
+  let initialMachine = (A, initTape, 0, 0)
+  runMachine initialMachine
